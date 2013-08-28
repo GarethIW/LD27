b741e4e590be08d934889204db213832a1412bcd
Gareth Williams <gareth@garethw.net>
2013-08-28 20:43:19 +0100

    Linux project changes

--- LD27.Mono.Linux/Properties/AssemblyInfo.cs
+++ LD27.Mono.Linux/Properties/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("SpatiumSecondorumDecum")]
+[assembly: AssemblyTitle("SpatiumSecondorumDecem")]
 [assembly: AssemblyProduct("LD27.Mono.Linux")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyDescription("")]
